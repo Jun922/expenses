a8aa08526f642473e2b3072db00da84a470a01a3
Jun922 <junjunchi620@gmail.com>
2024-05-07 21:22:44 +0900

    flask tutorial

--- manage.py
+++ manage.py
@@ -1,4 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    name = request.args.get("name")
+    content = ["hoge", "fuga", "fuge"]
+    return render_template("index.html", name=name, content=content)
+
+
+@app.route("/index", methods=["post"])
+def post():
+    content = ["hoge", "fuga", "fuge"]
+    name = request.form["name"]
+    content.append(name)
+    return render_template("index.html", name=name, content=content)
 
 
 if __name__ == "__main__":
